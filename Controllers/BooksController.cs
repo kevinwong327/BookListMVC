8cac8b94f4439100d17ae025aa3fc7a3cc400b5d
kevinwong327 <kevin.wong1@gmail.com>
2019-11-30 12:44:51 -0500

    Sec 5:52-Book Index View Part 1 (BookList Tab)

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Books.ToList());
         }
     }
 }
